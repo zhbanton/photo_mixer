7f8aeaeb2ac6877a5c33efa219b5f12723a541c1
zhbanton <zhbanton@gmail.com>
2014-07-26 22:46:28 -0400

    Added Votable module to Image and Comment models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ActiveRecord::Base
+  include Votable
   belongs_to :user
   belongs_to :image
   validates :body, presence: true
--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,5 @@
 class Image < ActiveRecord::Base
+  include Votable
   belongs_to :user
   has_many :comments
   validates :url, presence: true
