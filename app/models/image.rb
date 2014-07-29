5482ec99f46695ced5c127152f9f6cbfe5ada264
Emma Gardner <emmagardner@outlook.com>
2014-07-29 16:18:27 -0400

    made corrections to image model validations (image_path, not url, must be present).
    My images view is now styled.

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,6 +1,6 @@
 class Image < ActiveRecord::Base
   mount_uploader :image_path, ImageUploader
-  validates :url, presence: true
+  validates :image_path, presence: true
   validates :user_id, presence: true
 
   include Votable
