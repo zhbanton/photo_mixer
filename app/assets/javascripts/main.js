21df270035b0ee97b69e957064531bc55d7ec44a
Emma Gardner <emmagardner@outlook.com>
2014-07-29 18:13:23 -0400

    flash message are now visible on the site.

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,4 +1,10 @@
 $(document).on("page:change", function() {
   $('#new-comment').submit(createComment);
   $('#favorite-button').click(favorite);
+  $('.close').click(function(event){
+    $(this).parent().remove();
+    event.preventDefault();
+  });
+
 });
+
