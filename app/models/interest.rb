b68fd7dc8017bcc44410a9576d83c016feede945
zhbanton <zhbanton@gmail.com>
2014-07-30 17:15:03 -0400

    User can select interests- maps to tags model to access images tagged with their interests

--- app/models/interest.rb
+++ app/models/interest.rb
@@ -1,5 +1,11 @@
 class Interest < ActiveRecord::Base
   belongs_to :user
-  belongs_to :category
-  accepts_nested_attributes_for :category
+  belongs_to :tag
+  accepts_nested_attributes_for :tag
+
+  def tag_attributes=(tag_attributes)
+    tag = Tag.find_or_create_by(name: tag_attributes[:name].downcase)
+    self.tag_id = tag.id
+  end
+
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,4 @@
-class Category < ActiveRecord::Base
+class Tag < ActiveRecord::Base
   has_many :interests
   has_many :users, through: :interests
 end
