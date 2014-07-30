9145de9fac4079e5b56cb11a4aef421b9d6f9862
zhbanton <zhbanton@gmail.com>
2014-07-30 19:40:25 -0400

    Votes are destroyed when user deletes account

--- app/models/concerns/votable.rb
+++ app/models/concerns/votable.rb
@@ -5,7 +5,7 @@ module Votable
   extend ActiveSupport::Concern
 
   included do
-    has_many :votes, as: :votable
+    has_many :votes, as: :votable, dependent: :destroy
   end
 
   def voted_by?(user, direction)
