cf1ab1229ebc95e107aea8134fa436d3ce2fd951
zhbanton <zhbanton@gmail.com>
2014-07-31 20:29:58 -0400

    Added count method to category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -5,4 +5,24 @@ class Category < ActiveRecord::Base
   has_many :interests, dependent: :destroy
   has_many :users, through: :interests
 
+  def self.tag_counts
+    counts = {}
+    Tag.all.each do |tag|
+      if counts[tag.category.name]
+        counts[tag.category.name] += 1
+      else
+        counts[tag.category.name] = 1
+      end
+    end
+    counts
+  end
+
+  def count
+    tag_count = 0
+    Tag.all.each do |t|
+      tag_count += 1 if t.category_id == self.id
+    end
+    tag_count
+  end
+
 end
