2f99f3152a0efb98ee70232c3d5b144fe968b57c
Emma Gardner <emmagardner@outlook.com>
2014-08-14 16:03:13 -0400

    revised variable name in home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,10 +2,10 @@ class HomeController < ApplicationController
 
 def index
     if params[:filter]
-       @q = Image.search(params[:q])
+       @image_search = Image.search(params[:q])
        @images = Image.filterize(params[:filter])
     else
-      @q = Image.search(params[:q])
+      @image_search = Image.search(params[:q])
       @images = Image.filterize('all_time')
     end
   end
